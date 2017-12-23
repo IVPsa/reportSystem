3d2a13901c2373cc6ec750762c0275dc4f5c77b7
ivpsa <ivan_Vilugron@hotmail.es>
2017-12-23 19:55:11 -0400

    modelos

---
+++ app/rep_reporte.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class rep_reporte extends Model
+{
+    //
+    /**
+  * El nombre de la tabla en la BD
+  *
+  * @var string
+  */
+
+    protected $table ="REP_REPORTE"
+
+    /**
+   * Llave primaria de la tabla
+   *
+   * @var string
+   */
+  protected $primaryKey = "REP_COD";
+
+  /**
+   * Atributos que pueden ser asignados masivamente
+   *
+   * @var array
+   */
+      protected $fillable = [
+          'REP_DES',
+          'REP_FECHA',
+          'REP_ESTADO'
+      ];
+
+
+    public function ot_orden_trabajo()
+    {
+        return $this->hasOne('App\ot_orden_trabajo', '{OT_ID', 'OT_ORDEN_TRABAJO');
+    }
+
+    public function User()
+    {
+        return $this->hasOne('App\User', '{USER_ID', 'USERS');
+    }
+}
