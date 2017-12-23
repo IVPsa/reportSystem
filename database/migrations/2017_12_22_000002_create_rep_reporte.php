83a44d2389c9d8d78eb0f3548af0809e20d989d0
ivpsa <ivan_Vilugron@hotmail.es>
2017-12-23 19:25:26 -0400

    migraciones

---
+++ database/migrations/2017_12_22_000002_create_rep_reporte.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepReporte extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+
+        Schema::create('REP_REPORTE', function (Blueprint $table) {
+            $table->increments('REP_COD');
+            $table->string('REP_DES');
+            $table->date('REP_FECHA');
+            $table->string('REP_ESTADO');
+
+            $table->integer('REP_USER_ID')->unsigned();
+            $table->foreign('REP_USER_ID')->references('id')->on('users');
+
+            $table->integer('REP_OT_ID')->unsigned();
+            $table->foreign('REP_OT_ID')->references('OT_ID')->on('OT_ORDEN_TRABAJO');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
