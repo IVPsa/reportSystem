f8c698666145be0bff768a806ee3421944cebdee
ivpsa <ivan_Vilugron@hotmail.es>
2017-12-29 17:35:26 -0400

    nav bar

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+
+  <div class="col-md-12">
+    <h1>HOLA</h1>
+    <h2>HOLA</h2>
+    <h3>HOLA</h3>
+    <h4>HOLA</h4>
+    <h5>HOLA</h5>
+    <h6>HOLA</h6>
+
+</div>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,38 +4,53 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
+    <title>RPS</title>
 
     <!-- Styles -->
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <script>
+      function openNav() {
+          document.getElementById("mySidenav").style.width = "250px";
+      }
+
+      function closeNav() {
+          document.getElementById("mySidenav").style.width = "0";
+      }
+    </script>
+
+
 </head>
 <body>
-    <div id="app">
+    <div  id="app">
       @if (Route::has('login'))
         @auth
-        <nav class="navbar navbar-inverse navbar-static-top">
+        <nav class="navbar navbar-inverse " style="border-radius:0;">
             <div class="container">
                 <div class="navbar-header">
 
                     <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                    <button type="button" tyle="font-size:40px;" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
 
+                    <span class="hidden-xs navbar-left" style="font-size:40px; color:white; cursor:pointer" onclick="openNav()">&#9776;</span>
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+                    <h3 class="navbar-left">RPS</h3>
+
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
+
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
@@ -48,16 +63,24 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li  class="hidden-lg hidden-md">
+                                <a href="#">PERFIL</a>
+                            </li>
+                            <li  class="hidden-lg hidden-md">
+                                <a href="#">REPORTES</a>
+                            </li>
+                            <li  class="hidden-lg hidden-md">
+                                <a href="#">ORDENES DE TRABAJO</a>
+                            </li>
+
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                  <h4>  {{ Auth::user()->name }} <span class="caret"></span></h4>
                                 </a>
 
                                 <ul class="dropdown-menu">
                                     <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                             Logout
                                         </a>
 
@@ -67,18 +90,45 @@
                                     </li>
                                 </ul>
                             </li>
+
+
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
+
+
           @endauth
         @endif
+        
+      <div class="hidden-xs ">
 
-        @yield('content')
+        <div id="mySidenav" class="sidenav ">
+          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+          <a href="#">PERFIL</a>
+          <a href="#">REPORTES</a>
+          <a href="#">ORDENES DE TRABAJO</a>
+        </div>
+      </div>
+
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-xs-12">
+            @yield('content')
+          </div>
+        </div>
+
+      </div>
+
     </div>
+
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 </body>
 </html>
