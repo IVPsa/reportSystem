187a31f8e9ecc76491297a2884bff8d9698cfc27
ivpsa <ivan_Vilugron@hotmail.es>
2018-01-19 15:21:21 -0400

    ERRORES

--- database/migrations/2018_01_19_044729_create_empresa.php
+++ database/migrations/2018_01_19_044729_create_empresa.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Empresa extends Migration
+class CreateEmpresa extends Migration
 {
     /**
      * Run the migrations.
