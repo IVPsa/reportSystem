4e24a45785a4dee160702ea53b800d1d64745731
ivpsa <ivan_Vilugron@hotmail.es>
2018-03-06 22:24:08 -0400

    correciond e seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,15 +15,16 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-      'USU_NOMBRE' => $faker->name,
-      'email' => $faker->unique()->safeEmail,
+      'USU_NOMBRE' => 'ivan',
+      'email' => 'ivan_vilugron@hotmail.es',
       'password' => '123456',
       'remember_token' => str_random(10),
-      'USU_EMPRESA' => str_random(10),
-      'USU_TPU_COD' => str_random(10),
-      'USU_RUT' => str_random(10),
-      'USER_N_CTA_BANCO' => str_random(10),
-      'USER_BANCO' => str_random(10),
-      'USER_TP_CTA' => str_random(10)
+      'USU_EMPRESA' => 'mappu',
+      'USU_TPU_COD' => 'tecnico',
+      'USU_RUT' => '188745628',
+      'USER_N_CTA_BANCO' => '18874562',
+      'USER_BANCO' => 'banco estado',
+      'USER_AVATAR'=>'imagenes/sinPerfil.png',
+      'USER_TP_CTA' => 'RUT'
     ];
 });
