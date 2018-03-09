ebebe8d4d8e1fcde64088ed932577181414c4b09
ivpsa <ivan_Vilugron@hotmail.es>
2018-03-09 00:28:13 -0400

    meora de modal de foto de perfil

--- resources/views/modals/subirAvatar.blade.php
+++ resources/views/modals/subirAvatar.blade.php
@@ -10,27 +10,26 @@
       <div class="modal-body bg-dark">
         <div class="row">
 
-
           <div class="col-xs-12 col-md-12">
             <h3 class="text-center">SUBIR FOTO DE PERFIL</h3>
-            <div class="table-responsive">
+
               <form action="{{ route('subirAvatar')}}" method="post" enctype="multipart/form-data">
                   {{ csrf_field() }}
                   {{ method_field('PATCH') }}
 
-                  <table>
-                      <tr>
-                        <td>
-                          <input type="file" required   class="form-control" name="imagenPerfil" >
-                        </td>
-                        <td>
-                          <button type="submit" name="button" class="btn btn-success">Subir</button>
-                        </td>
-                      </tr>
-                  </table>
+                  <div class="input-group">
+                  <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="inputGroupFile04" name="imagenPerfil" required>
+                    <label class="custom-file-label" for="inputGroupFile04">Seleccionar imagen</label>
+                  </div>
+                  <div class="input-group-append">
+                    <button type="submit" name="button" class="btn btn-success">Subir</button>
+                  </div>
+                </div>
+
               </form>
 
-            </div>
+
           </div>
         </div>
       </div>
